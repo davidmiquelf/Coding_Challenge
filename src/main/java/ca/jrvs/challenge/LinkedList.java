efb9a7e8afe38395d544413b356520d3a2967b84
Davidmiquelf <david.faig.jrvs.ca@gmail.com>
2019-07-15 16:32:02 -0400

    Dealt with warnings.

--- src/main/java/ca/jrvs/challenge/LinkedList.java
+++ src/main/java/ca/jrvs/challenge/LinkedList.java
@@ -3,52 +3,61 @@ package ca.jrvs.challenge;
 import java.util.Collection;
 import java.util.Iterator;
 
+@SuppressWarnings({"unused", "WeakerAccess"})
 public class LinkedList<T> {
 
   private Link<T> head;
 
   public LinkedList(T val){
-    Link<T> link = new Link(val);
+    Link<T> link = new Link<>(val);
     this.setHead(link);
   }
-
   public LinkedList(Collection<T> collection){
     Iterator<T> iterator = collection.iterator();
     if (iterator.hasNext()){
-      this.setHead(new Link(iterator.next()));
+      this.setHead(new Link<>(iterator.next()));
     }
     Link<T> currentLink = this.getHead();
     while (iterator.hasNext()){
-      currentLink.setNext(new Link(iterator.next()));
+      currentLink.setNext(new Link<>(iterator.next()));
       currentLink = currentLink.getNext();
     }
-  }
-  public T get(int index) {
-    Link link = this.getLink(index);
-    return (T) link.getVal();
   }
 
   public Link<T> getHead() {
     return head;
   }
 
+  public void setHead(Link<T> head) {
+    this.head = head;
+  }
+
+  public T get(int index) {
+    Link<T> link = this.getLink(index);
+    if (link != null){
+      return link.getVal();
+    } else {
+      throw new IndexOutOfBoundsException();
+    }
+  }
+
   public void deleteAtIndex(int index){
     if (index == 0){
       this.setHead(this.getHead().getNext());
     }
-    Link link = this.getLink(index);
+    Link<T> link = this.getLink(index);
   }
 
 
   public void addAtHead(T val) {
-    Link newLink = new Link(val);
+    Link<T> newLink = new Link<>(val);
     newLink.setNext(this.getHead());
     this.setHead(newLink);
   }
 
   public void addAtTail(T val) {
-    Link newLink = new Link(val);
-    Link current = this.getHead();
+    Link<T> newLink = new Link<>(val);
+    Link<T> current = this.getHead();
     while (current.hasNext()) {
       current = current.getNext();
     }
@@ -56,19 +65,23 @@ public class LinkedList<T> {
   }
 
   public void addAtIndex(T val, int index) {
-    Link newLink = new Link(val);
+    Link<T> newLink = new Link<>(val);
     if (index == 0){
       newLink.setNext(this.getHead());
     } else {
-      Link link = getLink(index - 1);
-      newLink.setNext(link.getNext());
-      link.setNext(newLink);
+      Link<T> link = getLink(index - 1);
+      if (link != null){
+        if (link.hasNext()){
+          newLink.setNext(link.getNext());
+        }
+        link.setNext(newLink);
+      }
     }
   }
 
-  private Link getLink(int index){
+  private Link<T> getLink(int index){
     int idx = index;
-    Link link = this.getHead();
+    Link<T> link = this.getHead();
     if (idx < 0) {
       throw new IndexOutOfBoundsException();
     }
@@ -86,12 +99,10 @@ public class LinkedList<T> {
     return link;
   }
 
-  public void setHead(Link<T> head) {
-    this.head = head;
-  }
+
 
   public class Link<T> {
-    private Link next = null;
+    private Link<T> next = null;
     private T val;
 
     public Link(T val) {
@@ -99,11 +110,11 @@ public class LinkedList<T> {
     }
 
 
-    public Link getNext() {
+    public Link<T> getNext() {
       return next;
     }
 
-    public void setNext(Link next) {
+    public void setNext(Link<T> next) {
       this.next = next;
     }
 
